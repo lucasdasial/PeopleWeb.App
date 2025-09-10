8ef79e1106374b4fe139a1ee69668e9c10800492
Lucas Alves <as.lucasalves@gmail.com>
2025-09-10 03:02:01 -0400

    feat: create base form

---
+++ src/components/FormField.tsx
@@ -0,0 +1,23 @@
+interface FormFieldProps {
+  label: string;
+  required?: boolean;
+  error?: string;
+  children: React.ReactNode;
+}
+
+export const FormField: React.FC<FormFieldProps> = ({
+  children,
+  label,
+  required,
+  error,
+}) => {
+  return (
+    <div className="field">
+      <label className="form-label">
+        {label} {required && <span className="required">*</span>}
+        {children}
+        {error && <div className="form-error">{error}</div>}
+      </label>
+    </div>
+  );
+};
--- src/pages/AddPersonView.tsx
+++ src/pages/AddPersonView.tsx
@@ -2,6 +2,7 @@ import { ChevronLeft } from "lucide-react";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { FormField } from "../components/FormField";
 import { Head } from "../components/Head";
 import { Page } from "../components/Page";
 import { PeopleContext } from "../contexts/PeopleContext";
@@ -24,19 +25,58 @@ export const AddPersonView = () => {
       position: "top-left",
     });
 
+  function handleSubmit() {
+    navigate("/");
+    notify();
+    addPerson();
+  }
+
+  function handleCancel() {
+    navigate("/");
+  }
+
   return (
     <>
       <Page>
         <Head title="Adicionar pessoa" button={actionButton} isVariant />
-        <button
-          onClick={() => {
-            navigate("/");
-            notify();
-            addPerson();
-          }}
-        >
-          Cadastrar
-        </button>
+        <div className="form">
+          <h3>Campos Obrigatorios</h3>
+          <section className="fields fields-required">
+            <FormField label="Nome completo">
+              <input type="text" placeholder="Digite o nome completo" />
+            </FormField>
+            <FormField label="CPF">
+              <input type="number" placeholder="000.000.000-00" />
+            </FormField>
+            <FormField label="Data nascimento">
+              <input
+                type="date"
+                placeholder="Data nascimento"
+                className="date-picker"
+              />
+            </FormField>
+          </section>
+
+          <div className="divider" />
+          <h3>Campos Opicionais</h3>
+          <section className="fields fields-optional">
+            <FormField label="E-mail">
+              <input type="email" placeholder="email@exemplo.com" />
+            </FormField>
+            <FormField label="Naturalidade">
+              <input type="text" placeholder="Cidade de nascimento" />
+            </FormField>
+            <FormField label="Nacionalidade">
+              <input type="text" placeholder="Ex: Brasileira" />
+            </FormField>
+          </section>
+        </div>
+        <div className="form-buttons">
+          <button className="btn-cancel" onClick={() => handleCancel()}>
+            Cancelar
+          </button>
+          <button onClick={() => handleSubmit()}>Cadastrar</button>
+        </div>
       </Page>
     </>
   );
